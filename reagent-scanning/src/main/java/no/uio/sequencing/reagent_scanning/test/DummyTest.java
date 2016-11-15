5177cf8f707ebc91f0bf0426f74cffa199b014d5
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2016-11-15 12:13:58 +0100

    Moved test image to its own directory

--- reagent-scanning/src/main/java/no/uio/sequencing/reagent_scanning/test/DummyTest.java
+++ reagent-scanning/src/main/java/no/uio/sequencing/reagent_scanning/test/DummyTest.java
@@ -10,7 +10,7 @@ import javax.imageio.ImageIO;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
-import no.uio.sequencing.reagent_scanning.ScanResultsSaver;
+import no.uio.sequencing.reagent_scanning.ScanResultsWorkflow;
 
 public class DummyTest {
 
@@ -20,9 +20,9 @@ public class DummyTest {
 		arx.add("1202012");
 		arx.add("1202012");
 		System.out.println(new File( "." ).getCanonicalPath());
-		ScanResultsSaver srss = new ScanResultsSaver(arx);
+		ScanResultsWorkflow srss = new ScanResultsWorkflow(arx);
 		
-		File fooFile = new File("resources/test_image.png");
+		File fooFile = new File("resources/test/test_image.png");
 		BufferedImage foo = ImageIO.read(fooFile);
 		ITesseract tess = new Tesseract();
 		System.out.println(tess.doOCR(foo));
